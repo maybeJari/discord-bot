459107dc1cf5c79629df03698d906de4fb0843aa
Jari <34107546+notJari@users.noreply.github.com>
2021-11-01 16:33:47 +0200

    Create eval.ts

---
+++ commands/owner/eval.ts
@@ -0,0 +1,35 @@
+import { ICommand } from "wokcommands";
+const { MessageEmbed } = require("discord.js");
+
+export default {
+    category: 'Owner',
+    description: 'Eval',
+
+    slash: 'both',
+    testOnly: false,
+    ownerOnly: true,
+
+    callback: ({ client, message, args }) => {
+      try {
+        const code = args.join(" ");
+        if (!code) {
+           return message.channel.send("What do you want to evaluate?")
+        }
+        
+        let evaled = eval(code);
+
+        if (typeof evaled !== "string")
+          evaled = require("util").inspect(evaled);
+
+          let embed = new MessageEmbed()
+          .setAuthor("Eval", message.author.avatarURL())
+          .addField("Input", `\`\`\`${code}\`\`\``)
+          .addField("Output", `\`\`\`${evaled}\`\`\``)
+          .setColor("GREEN")
+
+        message.channel.send({embeds: [embed]});
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`\n${err}\`\`\``);
+}
+    },
+} as ICommand
